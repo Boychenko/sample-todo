c0310bffd3d3facd74d49ba7910f72311ce23b6c
Alexander Boychenko <boychenkoalexander@gmail.com>
2016-03-28 22:37:39 +0700

    Adds test on title for about page. Updates dependencies

--- clients/react/src/components/AboutPage.spec.js
+++ clients/react/src/components/AboutPage.spec.js
@@ -2,6 +2,7 @@ import chai from 'chai';//eslint-disable-line no-unused-vars
 import cheerio from 'cheerio';
 import AboutPage from './AboutPage';
 import React from 'react';
+import Helmet from 'react-helmet';
 import ReactDOMServer from 'react/lib/ReactDOMServer';
 
 chai.should();
@@ -18,6 +19,19 @@ describe('About page', () => {
 
     //assert
     header.should.equal('About us');
+  });
+
+  it('Should have title \'About us\'', () => {
+    //arrange
+    const sut = React.createElement(AboutPage);
+
+    //act
+    ReactDOMServer.renderToStaticMarkup(sut);
+    const head = Helmet.rewind();
+    const title = head.title.toComponent()[0].props.children;
+
+    //assert
+    title.should.equal('About us');
   });
 });
 
