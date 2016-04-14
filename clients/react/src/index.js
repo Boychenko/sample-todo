21e7ee96ff8fc2ece22438bc2f5d50f2013882fa
Alexander Boychenko <boychenkoalexander@gmail.com>
2016-04-14 22:37:00 +0700

    Adds react-router-redux

--- clients/react/src/index.js
+++ clients/react/src/index.js
@@ -5,12 +5,14 @@ import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import configureStore from './store/configureStore';
+import {syncHistoryWithStore} from 'react-router-redux';
 import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.]
 
 const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
 
 render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes}/>
+    <Router history={history} routes={routes}/>
   </Provider>, document.getElementById('app')
 );
--- clients/react/src/reducers/index.js
+++ clients/react/src/reducers/index.js
@@ -2,16 +2,18 @@ import {combineReducers} from 'redux';
 import * as types from '../constants/ActionTypes';
 import {reducer as formReducer} from 'redux-form';
 import paginate from './paginate';
+import {routerReducer} from 'react-router-redux';
 
 const rootReducer = combineReducers({
-  items: paginate({
+  items  : paginate({
     types: [
       types.ITEMS_REQUEST,
       types.ITEMS_SUCCESS,
       types.ITEMS_FAILURE
     ]
   }),
-  form : formReducer
+  form   : formReducer,
+  routing: routerReducer
 });
 
 export default rootReducer;
